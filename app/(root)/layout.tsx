f6507117213af0f3e62ff5ecce67d7d6ceb16547
Thai Huynh <rafehuynh0510@gmail.com>
2024-11-14 20:56:28 -0600

    sidebar, header

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import MobileNavigation from '../components/MobileNavigation'
+import Header from '../components/Header'
+import { getUser } from '@/lib/actions/user.action'
+import { redirect } from 'next/navigation'
+
+const layout = async ({children} : {children: React.ReactNode}) => {
+    const user = await getUser()
+    if(!(user)){
+        return redirect('/sign-in')
+    }
+  return (
+    <main className='flex h-screen'>
+        <Sidebar {...user}/>
+        <section className='flex h-full flex-1 flex-col'>
+            <MobileNavigation />
+            <Header/>
+            <div className='main-content'>
+                {children}
+            </div>
+        </section>
+    </main>
+  )
+}
+
+export default layout
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { createAdminClient } from "@/appwrite"
+import { createAdminClient, createSessionClient } from "@/appwrite"
 import { appwriteConfig } from "@/appwrite/config"
 import { ID, Query } from "node-appwrite"
 import { string } from "zod"
@@ -74,3 +74,14 @@ export const verifySecret = async ({
       handleError(error, "Failed to verify OTP");
     }
   };
+export const getUser = async() => {
+  const {databases, account} = await createSessionClient()
+  const result = await account.get()
+  const user = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("accountId", result.$id)]
+  )
+  if(user.total <= 0)return null
+  return parseStringify(user.documents[0])
+}
