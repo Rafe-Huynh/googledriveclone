25557551081b30cba58a5306271a11dbd841fc14
Thai Huynh <rafehuynh0510@gmail.com>
2024-12-05 19:41:53 -0600

    recent file upload

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,9 @@ import Image from 'next/image'
 import { Separator } from '@radix-ui/react-separator'
 import FormattedDateTime from '../components/FormattedDateTime'
 import { Chart } from '../components/Chart'
+import { Models } from 'node-appwrite'
+import Thumbnail from '../components/Thumbnail'
+import ActionDropdown from '../components/ActionDropdown'
 
 const Dashboard = async () => {
   const [files, totalSpace] = await Promise.all([
@@ -40,6 +43,40 @@ const Dashboard = async () => {
             ))
           }
         </ul>
+      </section>
+      <section className="dashboard-recent-files">
+        <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
+        {files.documents.length > 0 ? (
+          <ul className="mt-5 flex flex-col gap-5">
+            {files.documents.map((file: Models.Document) => (
+              <Link
+                href={file.url}
+                target="_blank"
+                className="flex items-center gap-3"
+                key={file.$id}
+              >
+                <Thumbnail
+                  type={file.type}
+                  extension={file.extension}
+                  url={file.url}
+                />
+
+                <div className="recent-file-details">
+                  <div className="flex flex-col gap-1">
+                    <p className="recent-file-name">{file.name}</p>
+                    <FormattedDateTime
+                      date={file.$createdAt}
+                      className="caption"
+                    />
+                  </div>
+                  <ActionDropdown file={file} />
+                </div>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <p className="empty-list">No files uploaded</p>
+        )}
       </section>
     </div>
   )
